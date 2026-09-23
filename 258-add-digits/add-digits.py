4171084418dc2d19d14f9175469e7be459e4f7a2
Sindhusaranti <sindhusaranti369@gmail.com>
2026-09-23 15:22:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.67%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        sum=0
+        while num!=0:
+            sum=sum+num%10
+            num=num//10
+        if sum>=10:
+            return self.addDigits(sum)
+        return sum
